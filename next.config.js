813b082038efafed61cebabeb96554ea68799241
Geury <theromantic2010@hotmail.com>
2023-04-08 18:36:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    NEXTRUNTIME: "development"
-  },
+  // env: {
+  //   NEXTRUNTIME: "development"
+  // },
   images: {
     remotePatterns: [
       {
