af58d595419b3d13b2d8150556c73d7c2baf2ff5
Geury <theromantic2010@hotmail.com>
2023-04-08 12:58:48 +0200

    Update Awards.jsx

--- src/Components/Awards/Awards.jsx
+++ src/Components/Awards/Awards.jsx
@@ -1,27 +1,32 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { useRouter } from "next/router";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
 import Container from "react-bootstrap/Container";
 
 import styled from "./Awards.module.css";
-import FallBackLoading from "../Loading/FallBackLoading";
+// import FallBackLoading from "../Loading/FallBackLoading";
+// import FallBackLoading from "../Loading/FallBackLoading";
+import TripAdVisor from "./TripAdVisor";
 
-const DynamicTripAdVisor = dynamic(() => import("./TripAdVisor"), {
-  suspense: true
-});
+// const DynamicTripAdVisor = dynamic(() => import("./TripAdVisor"), {
+//   suspense: true
+// });
 
 const Awards = () => {
   const router = useRouter();
 
   const addCSS =
     router.asPath !== "/" ? `${styled.awards} ${styled.awardsWithBorder}` : `${styled.awards}`;
+
   return (
     <section className={addCSS}>
       <Container>
-        <Suspense fallback={<FallBackLoading />}>
+        <TripAdVisor />
+
+        {/* <Suspense fallback={<FallBackLoading />}>
           <DynamicTripAdVisor />
-        </Suspense>
+        </Suspense> */}
       </Container>
     </section>
   );
