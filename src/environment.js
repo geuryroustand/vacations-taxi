18a30ce7819db8fc7c6148585d0f052d36141537
Geury <theromantic2010@hotmail.com>
2023-09-23 10:50:05 +0200

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,1 @@
-const environmentVariables = {
-  NEXT_PUBLIC_API_STRAPI_DEV_URL: process.env.NEXT_PUBLIC_API_STRAPI_DEV_URL
-};
-
-// eslint-disable-next-line import/prefer-default-export
-export const { NEXT_PUBLIC_API_STRAPI_DEV_URL } = environmentVariables;
+export const { NEXT_PUBLIC_STRIPE_SECRET, NEXT_PUBLIC_API_STRAPI_DEV_URL } = process.env;
