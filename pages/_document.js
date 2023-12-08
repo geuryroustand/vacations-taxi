0e37917c11d052257c2c05d4debd4066a02530d2
Geury <theromantic2010@hotmail.com>
2023-12-08 11:34:05 +0100

    added google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,16 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${
+                process.env.NODE_ENV === "production" &&
+                process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
+              }"height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
+          {/* <!-- End Google Tag Manager (noscript) --> */}
         </body>
       </Html>
     );
