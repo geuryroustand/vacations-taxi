d4fefc37663adbfa16db5d59f24fa75842e14369
Geury <theromantic2010@hotmail.com>
2024-03-29 16:42:22 +0100

    Update FormRegisterAndSign.jsx

--- src/Components/FormRegisterAndSign/FormRegisterAndSign.jsx
+++ src/Components/FormRegisterAndSign/FormRegisterAndSign.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { ImFacebook2 } from "react-icons/im";
 import { FcGoogle } from "react-icons/fc";
 import styled from "./FormRegisterAndSign.module.css";
-import { NEXT_PUBLIC_API_STRAPI_DEV_URL } from "../../../environment";
+import { baseURL } from "../../../environment";
 
 const FormRegisterAndSign = ({ heading, facebookBtnText, googleBtnText, children, or }) => {
   return (
     <div className={styled.main}>
       <h1 className={styled.heading}>{heading}</h1>
       <a
-        href={`${NEXT_PUBLIC_API_STRAPI_DEV_URL}/connect/google/redirect`}
+        href={`${baseURL}/connect/google/redirect`}
         type="submit"
         className={`${styled.googleBtn}  ${styled.socialBtn}`}>
         <FcGoogle className={styled.icons} />
         {googleBtnText}
       </a>
       <a
-        href={`${NEXT_PUBLIC_API_STRAPI_DEV_URL}/connect/facebook/redirect`}
+        href={`${baseURL}/connect/facebook/redirect`}
         className={`${styled.facebookIcon}  ${styled.socialBtn}`}
         type="submit">
         <ImFacebook2 className={styled.icons} />
